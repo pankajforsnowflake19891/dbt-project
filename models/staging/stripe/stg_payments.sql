f4936e7ba619549b067a975744e5c45210fa5601
pankaj wagh <pankajforsnowflake19891@gmail.com>
2024-08-14 10:05:52 +0000

    tests

--- models/staging/stripe/stg_payments.sql
+++ models/staging/stripe/stg_payments.sql
@@ -1,6 +1,7 @@
 select
     id as payment_id,
-    orderid,
-    amount,status
+    orderid as order_id,
+    paymentmethod as payment_status,
+    amount,status,created,_batched_at
 
-from raw.stripe.payment
+from {{ source('stripe','payment') }}
