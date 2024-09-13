97106e51345e7594827c8fc51b9659664ac73dff
pankaj wagh <pankajforsnowflake19891@gmail.com>
2024-09-13 05:02:37 +0000

    commit

--- models/DataVault/hub/hub_customer.sql
+++ models/DataVault/hub/hub_customer.sql
@@ -1,4 +1,7 @@
-{{ config(materialized='incremental') }}
+{{ config(
+    materialized='incremental',
+    unique_key = 'customer_hk'
+    ) }}
 
 SELECT
     MD5_number_upper64(customer_id) AS customer_hk,
@@ -6,3 +9,11 @@ SELECT
     CURRENT_TIMESTAMP() AS load_dts,
     'RAW' AS source
 FROM {{ ref('stg_customers') }}
+
+{% if is_incremental() %}
+
+  -- this filter will only be applied on an incremental run
+  -- (uses >= to include records whose timestamp occurred since the last run of this model)
+where load_dts >= (select max(load_dts)) from {{ this }} )
+
+{% endif %}
