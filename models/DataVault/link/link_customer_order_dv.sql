83efb46db41892a431ff4d871a7a303f2a1ad4f3
pankaj wagh <pankajforsnowflake19891@gmail.com>
2024-09-13 08:26:29 +0000

    commit

--- models/DataVault/link/link_customer_order_dv.sql
+++ models/DataVault/link/link_customer_order_dv.sql
@@ -1,4 +1,4 @@
-{%- set source_model = "link_customer_order" -%}
+{%- set source_model = "stg_orders_dv" -%}
 {%- set src_pk = "customer_order_hk" -%}
 {%- set src_fk = ["customer_hk", "order_hk"] -%}
 {%- set src_ldts = "load_dts" -%}
