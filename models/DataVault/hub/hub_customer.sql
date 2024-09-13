ed676db7c712a1dd9331ce59f18f839fc4f6eae5
pankaj wagh <pankajforsnowflake19891@gmail.com>
2024-09-13 05:09:01 +0000

    commit

--- models/DataVault/hub/hub_customer.sql
+++ models/DataVault/hub/hub_customer.sql
@@ -9,11 +9,3 @@ SELECT
     CURRENT_TIMESTAMP() AS load_dts,
     'RAW' AS source
 FROM {{ ref('stg_customers') }}
-
-{% if is_incremental() %}
-
-  -- this filter will only be applied on an incremental run
-  -- (uses >= to include records whose timestamp occurred since the last run of this model)
-where load_dts >= (select max(load_dts)) from {{ this }} )
-
-{% endif %}
